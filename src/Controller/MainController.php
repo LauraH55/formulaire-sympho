ce930edd9cc2d4254da6ae94d08f485ee421ef6b
Laura Hantz <laura.hantz@hotmail.fr>
2021-02-25 15:24:27 +0100

    traitement de base du form

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -2,27 +2,44 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\Form\Extension\Core\Type\EmailType;
-use Symfony\Component\Form\Extension\Core\Type\PasswordType;
-use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use App\Form\LoginType;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class MainController extends AbstractController
 {
     /**
      * @Route("/", name="home")
      */
-    public function index(): Response
-    {   
+    public function index(Request $request): Response
+    {
         // 1. Directement dans le contrôleur
-        // On créé un formulaire de login
-        $form = $this->createFormBuilder()
-            ->add('email', EmailType::class)
-            ->add('pasword', PasswordType::class)
-            ->add('submit', SubmitType::class)
-            ->getForm();
+        // On crée un formulaire de login
+        $form = $this->createForm(LoginType::class,
+            // Valeurs par défaut des champs du form
+            ['user_email' => 'toto@toto.com']
+            // Options du formulaire
+            // ['action' => 'http://toto.com/login']
+        );
+
+        //2. Demande au formulaire d'inspecter la requête 
+        // et de faire ce qu'il y a à faire
+        $form->handleRequest($request);
+
+        // Le formulaire est-il soumis et valide ? 
+        if ($form->isSubmitted() && $form->isValid()) {
+
+            // On récupère les données du form
+            $loginData = $form->getData();
+
+            // Fait quelque chose => se connecter
+            dd($loginData);
+
+            // On redirige vers ....
+            // return $this->redirectToRoute(...)
+        }
 
         return $this->render('main/index.html.twig', [
             // On envoie au template "une vue de formulaire" via createView()
---
+++ src/Form/LoginType.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+
+class LoginType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+        ->add('user_email', EmailType::class, [
+            // Supprimer le required HTML5
+            'required' => false,
+        ])
+        ->add('password', PasswordType::class, [
+            // Supprimer le required HTML5
+            'required' => false,
+        ])
+        ->add('submit', SubmitType::class)
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            // Configure your form options here
+        ]);
+    }
+}
